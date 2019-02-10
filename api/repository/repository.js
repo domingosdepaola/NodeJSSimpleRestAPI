9aee55125481aa43183fedc9bb4ca29fb693122d
Domingos De Paola <domingosdepaola@gmail.com>
2019-02-10 18:28:46 +0000

    adicional developments

--- api/repository/repository.js
+++ api/repository/repository.js
@@ -1,6 +1,17 @@
 exports.execSQLQuery = function (query, res) {
 
     global.conn.request()
-        .query(query).then(result => res.json(result.recordset))
+        .query(query).then(result => {
+            var objReturnLst = [];
+            result.recordset.forEach(element => {
+                var obj = { Id : element.ID, 
+                            Nome: element.NOME,
+                            Matricula: element.MATRICULA,
+                            Expiracao: element.EXPIRACAO  
+                          }
+                objReturnLst.push(obj);
+            });
+            res.json(objReturnLst) 
+        })
         .catch(err => res.json(err));
 }
